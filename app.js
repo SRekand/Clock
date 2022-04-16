3a5bdcdc69f7725a6069678428f91864f08d901e
Steven Kristjan <s.rekand@voco.ee>
2022-04-16 16:00:32 +0200

    Finished Clock

---
+++ app.js
@@ -0,0 +1,28 @@
+function showtime() {
+    var date = new Date()
+    var h = date.getHours()
+    var m = date.getMinutes()
+    var s = date.getSeconds()
+    var session = "AM"
+
+    if(h == 0) {
+        h = 12
+    }
+    if(h > 12) {
+        h = h - 12
+        session = "PM"
+    }
+
+    h = (h < 10) ? "0" + h : h
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+    
+    var text = h + ":" + m + ":" + s + " " + session
+
+    document.getElementById('clockDisplay').innerText = text
+    document.getElementById('clockDisplay').textContent = text
+
+    setTimeout(showtime, 1000)
+}
+
+showtime()
